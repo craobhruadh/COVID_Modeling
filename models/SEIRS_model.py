e23b26969b80254433714c6869131657928aecc2
Ross <publius314@gmail.com>
2022-03-11 23:38:48 -0800

    updating models

--- models/SEIRS_model.py
+++ models/SEIRS_model.py
@@ -1,11 +1,47 @@
+from data_helpers import POPULATION_OF_LA
 import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.optimize import minimize
+from dataclasses import dataclass
 
-# Number contacts per day per person that will lead to an infection
-# unknown but we can guess and then find out from the data
-# For now using Usherwood et. al. guess of 0.35
-BETA_INITIAL_GUESS = 0.35
 
-# Gamma is 1/number of days of infectiousness
-GAMMA_INITIAL_GUESS = 0.10
+@dataclass
+class SEIRS_parameters:
+    """Parameters:
+
+    alpha: Virus induced average fatality rate
+    beta: Probability of transmission per contact times the number of contacts per unit of time
+    epsilon: Rate of progression from exposed to infectious (1/incubation period)
+    gamma: Recovery rate of individual
+    """
+
+    population: int = POPULATION_OF_LA
+    suceptible: int = POPULATION_OF_LA
+    exposed: int = 0
+    infected: int = 0
+    recovered: int = 0
+    dead: int = 0
+
+    alpha: float = 0.01
+    beta: float = 0.01
+    epsilon: float = 0.01
+    gamma: float = 0.01
+
+
+# Sample usage:
+#
+# params = SEIRS_parameters(beta=0.05, gamma=0.01, lambda_param =0.001)
+# print(params)
+# params.beta = 0.025
+# print(params)
+
+
+def SEIRS(params):
+    """Wrapper for the scipy solve_ivp routine and the
+    SEIRS function/model
+
+    params: a SEIRS_parameters dataclass"""
+
+    dS_dt = -params.beta * params.infected * params.suceptible / params.population
+    # dE_dt = - 
+
